7fe8c64b11ceaa201ec266507eb6b66e727815b9
imanol309 <imanolguzman309@gmail.com>
2021-05-28 17:04:12 -0400

    version 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React,{useState}from 'react';
+import './app.css'
 
+const gif = ['https://media3.giphy.com/media/LMrQR7qJXg0UoMquoI/giphy.gif',
+'https://media1.giphy.com/media/5SAPzzr9MUdpW1jXQA/giphy.gif?cid=ecf05e473qeo4rbtgvp18a241kvj43o8r0fsij6wrddgp33j&rid=giphy.gif&ct=g']
+
+const gifActualizando = ["https://media1.giphy.com/media/PWVDv5lvxJc40KCLK5/giphy.gif?cid=ecf05e47pksobct3r2wrb69opzs3du8unyoakeonwpvwafa8&rid=giphy.gif&ct=g"]
 
 function App() {
+
+  const [valor, valorActualizado] = useState(gif)
+
   return (
     <div className="App">
-      <p>hola</p>
+       <section className="contenedor-app">
+         {
+           valor.map(dosGif => <img src={dosGif} alt="" />)
+         }
+       </section>
+       <button onClick={() => valorActualizado(gifActualizando)}>
+          Cambiar Nuevo
+        </button>
     </div>
   );
 }
